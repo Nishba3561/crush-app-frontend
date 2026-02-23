14e09c830020769a2952042c99ba7d20411885de
Nishba3561 <nishba3561@gmail.com>
2026-02-23 20:09:13 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "Crush Thoughts",
+  "short_name": "CrushApp",
+  "description": "Discover what your crush thinks about you!",
+  "start_url": "/index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#e91e8c",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
